38747067b481d6c9f54e8785b8a43aaa2f7bcaf3
Tim de kok <1040606@mborijnland.nl>
2019-05-10 12:41:58 +0200

    update 2

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,6 +1,7 @@
 USE `lo8e_sql2`;
 
-INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES
-('lo9e-amo1', 'akr02', '2019', 'amo'),
-('lo9e-amo1', 'rs001', '2019', 'amo'),
-('lo9e-amo1', 'bkr02', '2019', 'amo');
+INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam) VALUES
+('98765', 'Grote', 'de', 'Tim'),
+('99999', 'Grote', 'de', 'Mitchell'),
+('90000', 'Grote', 'de', 'Moied'),
+('91000', 'Grote', 'de', 'Nick');
