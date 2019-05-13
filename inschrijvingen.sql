61b9a5e8ba95a6f0e55e4480ee114d35d11259af
Tim de kok <1040606@mborijnland.nl>
2019-05-13 16:34:37 +0200

    update 4

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,7 +1,5 @@
 USE `lo8e_sql2`;
 
-INSERT INTO klassen_studenten (klas_code, ov_nummer) VALUES
-('lo9e-amo1','98765'),
-('lo9e-amo2','99999'),
-('lo9e-amo3','90000'),
-('lo9e-amo3','91000');
+UPDATE studenten 
+SET geboortedatum = 05-07-2002
+WHERE voornaam = Tim;
