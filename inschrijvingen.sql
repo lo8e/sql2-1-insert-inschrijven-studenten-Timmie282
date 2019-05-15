4ac182965062ab88a66601c80897c8d4ba27f5d0
Tim de kok <1040606@mborijnland.nl>
2019-05-15 14:06:12 +0200

    update 6

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,4 +1,8 @@
 USE `lo8e_sql2`;
 
-SET actief = '0'
-WHERE inschrijvings_datum <= '2017-01-01'
+SELECT studenten.ov_nummer, CONCAT(studenten.voornaam, ' ' , studenten.tussenvoegsel, ' ', studenten.achternaam), klassen.klas_code, klassen.cohort, CONCAT(docenten.voorletters,' ', docenten.tussenvoegsel, ' ', docenten.achternaam)
+FROM studenten
+LEFT JOIN klassen_studenten ON klassen_studenten.ov_nummer = studenten.ov_nummer
+LEFT JOIN klassen ON klassen.klas_code = klassen_studenten.klas_code
+LEFT JOIN docenten ON docenten.docent_code = klassen.klas_code
+ORDER BY studenten.achternaam, klassen.klas_code;
